43fe2f9c461912fafde4792cfe9be31720514153
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2022-04-14 23:22:28 +0530

    updated the ngdatatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {DataTablesModule} from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/ng-datatable/ng-datatable.component.ts
+++ src/app/users/ng-datatable/ng-datatable.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { UserService } from 'src/app/_services/user.service';
+
 
 @Component({
   selector: 'app-ng-datatable',
@@ -7,8 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgDatatableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userservice: UserService) {
+  }
+
+  dtOptions: DataTables.Settings = {}
+  dtTriggers: Subject<any> = new Subject<any>()
+  allusers: any[] = []
+  displayTable: boolean = false;
+
+  ngAfterViewInit(): void {
+    this.dtTriggers.next();
+  }
 
   ngOnInit(): void {
+
+    this.userservice.getData('https://jsonplaceholder.typicode.com/users').subscribe((response: any) => {
+      this.allusers = response
+      this.dtOptions = response;
+      this.dtTriggers.next()
+      console.log(this.dtTriggers)
+      this.displayTable = true
+    })
+  }
+
+  ngOnDestroy() {
+    this.dtTriggers.unsubscribe()
   }
 }
